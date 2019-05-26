dd525fa377a50dc257d12e83c8a3a2579320f4e3
David Boydell <david.boydell@bbc.co.uk>
2019-05-26 19:07:43 +0100

    creating npm package, bump version

--- index.js
+++ index.js
@@ -43,21 +43,75 @@ Cypress.Commands.add('isNotBottomAlignedWith', { prevSubject: true }, (subject, 
   return subject;
 }));
 
-Cypress.Commands.add('isBelow', { prevSubject: true }, (subject, element = undefinedElement(), length = undefinedLength()) => isPositioned(subject, 'below', element, length).then(subj => subj));
+Cypress.Commands.add('isBelow', { prevSubject: true }, (subject, element = undefinedElement(), length = undefinedLength()) => isPositioned(subject, 'below', element, length).then((subj) => {
+  expect(subj[0], subj[1]).to.equal(true);
+  return subject;
+}));
 
-Cypress.Commands.add('isAbove', { prevSubject: true }, (subject, element = undefinedElement(), length = undefinedLength()) => isPositioned(subject, 'above', element, length).then(subj => subj));
+Cypress.Commands.add('isAbove', { prevSubject: true }, (subject, element = undefinedElement(), length = undefinedLength()) => isPositioned(subject, 'above', element, length).then((subj) => {
+  expect(subj[0], subj[1]).to.equal(true);
+  return subject;
+}));
 
-Cypress.Commands.add('isLeftOf', { prevSubject: true }, (subject, element = undefinedElement(), length = undefinedLength()) => isPositioned(subject, 'left of', element, length).then(subj => subj));
+Cypress.Commands.add('isLeftOf', { prevSubject: true }, (subject, element = undefinedElement(), length = undefinedLength()) => isPositioned(subject, 'left of', element, length).then((subj) => {
+  expect(subj[0], subj[1]).to.equal(true);
+  return subject;
+}));
 
-Cypress.Commands.add('isRightOf', { prevSubject: true }, (subject, element = undefinedElement(), length = undefinedLength()) => isPositioned(subject, 'right of', element, length).then(subj => subj));
+Cypress.Commands.add('isRightOf', { prevSubject: true }, (subject, element = undefinedElement(), length = undefinedLength()) => isPositioned(subject, 'right of', element, length).then((subj) => {
+  expect(subj[0], subj[1]).to.equal(true);
+  return subject;
+}));
 
-Cypress.Commands.add('hasWidthOf', { prevSubject: true }, (subject, length = undefinedLength(), relativeTo) => dimensionRelativeTo(subject, 'width', relativeTo, length).then(subj => subj));
+Cypress.Commands.add('isNotBelow', { prevSubject: true }, (subject, element = undefinedElement(), length = undefinedLength()) => isPositioned(subject, 'below', element, length).then((subj) => {
+  expect(subj[0], subj[1]).to.equal(true);
+  return subject;
+}));
 
-Cypress.Commands.add('hasHeightOf', { prevSubject: true }, (subject, length = undefinedLength(), relativeTo) => dimensionRelativeTo(subject, 'height', relativeTo, length).then(subj => subj));
+Cypress.Commands.add('isNotAbove', { prevSubject: true }, (subject, element = undefinedElement(), length = undefinedLength()) => isPositioned(subject, 'above', element, length).then((subj) => {
+  expect(subj[0], subj[1]).to.equal(true);
+  return subject;
+}));
 
-Cypress.Commands.add('isInside', { prevSubject: true }, (subject, element = undefinedElement(), expected = undefinedObject()) => {
-  isInside(subject, element, expected).then(subj => subj);
-});
+Cypress.Commands.add('isNotLeftOf', { prevSubject: true }, (subject, element = undefinedElement(), length = undefinedLength()) => isPositioned(subject, 'left of', element, length).then((subj) => {
+  expect(subj[0], subj[1]).to.equal(true);
+  return subject;
+}));
+
+Cypress.Commands.add('isNotRightOf', { prevSubject: true }, (subject, element = undefinedElement(), length = undefinedLength()) => isPositioned(subject, 'right of', element, length).then((subj) => {
+  expect(subj[0], subj[1]).to.equal(true);
+  return subject;
+}));
+
+Cypress.Commands.add('hasWidthOf', { prevSubject: true }, (subject, length = undefinedLength(), relativeTo) => dimensionRelativeTo(subject, 'width', relativeTo, length).then((subj) => {
+  expect(subj[0], subj[1]).to.equal(true);
+  return subject;
+}));
+
+Cypress.Commands.add('hasHeightOf', { prevSubject: true }, (subject, length = undefinedLength(), relativeTo) => dimensionRelativeTo(subject, 'height', relativeTo, length).then((subj) => {
+  expect(subj[0], subj[1]).to.equal(true);
+  return subject;
+}));
+
+Cypress.Commands.add('doesNotHaveWidthOf', { prevSubject: true }, (subject, length = undefinedLength(), relativeTo) => dimensionRelativeTo(subject, 'width', relativeTo, length).then((subj) => {
+  expect(subj[0], subj[1]).to.equal(false);
+  return subject;
+}));
+
+Cypress.Commands.add('doesNotHaveHeightOf', { prevSubject: true }, (subject, length = undefinedLength(), relativeTo) => dimensionRelativeTo(subject, 'height', relativeTo, length).then((subj) => {
+  expect(subj[0], subj[1]).to.equal(false);
+  return subject;
+}));
+
+Cypress.Commands.add('isInside', { prevSubject: true }, (subject, element = undefinedElement(), expected = undefinedObject()) => isInside(subject, element, expected).then((subj) => {
+  expect(subj[0], subj[1]).to.equal(true);
+  return subject;
+}));
+
+Cypress.Commands.add('isNotInside', { prevSubject: true }, (subject, element = undefinedElement(), expected = undefinedObject()) => isInside(subject, element, expected).then((subj) => {
+  expect(subj[0], subj[1]).to.equal(false);
+  return subject;
+}));
 
 Cypress.Commands.add('waitFor', { prevSubject: false }, (element = undefinedElement()) => {
   cy.get(element);
