ece26020509cf05ebd1345708d768f251344985a
David Boydell <boyded01@mc-n348409.local>
2019-11-07 16:02:37 +0000

    Adding positioning

--- cypress/integration/test.js
+++ cypress/integration/test.js
@@ -1,9 +1,52 @@
-it('uses a custom chai helper', () => {
-  cy.viewport(1680, 1050);
-  cy.visit('https://www.bbc.co.uk/news');
-  cy.get('#orb-modules > header').should('be.leftAligned', '#orb-modules');
-  cy.get('#nw-c-topstories-domestic').should('be.rightAligned', '#orb-modules > header');
-  cy.get('#nw-c-topstories-domestic').should('not.be.rightAligned', 'nav.nw-c-nav__wide');
-  cy.get('.nw-c-top-stories__primary-item').should('be.topAligned', 'div.nw-c-top-stories__secondary-item--1');
-  cy.get('.nw-c-top-stories__primary-item').should('be.bottomAligned', 'div.nw-c-top-stories__secondary-item--1');
+describe('Chai plugin', () => {
+  before(() => {
+    cy.viewport(1680, 1050);
+    cy.visit('https://www.bbc.com/pidgin');
+  });
+
+  context('Alignment', () => {
+    it('asserts left alignment', () => {
+      cy.get('nav > div > ul')
+        .should('be.leftAligned', '#root > header > div > div')
+        .should('not.be.leftAligned', '#root > header > nav > div > ul > li:nth-child(9)');
+    });
+
+    it('asserts right alignment', () => {
+      cy.get('section[aria-labelledby="Top-stories"] li:nth-child(1)')
+        .should('be.rightAligned', 'section[aria-labelledby="Top-stories"] li:nth-child(2)')
+        .should('not.be.rightAligned', '#root > header > nav > div > ul > li:nth-child(9)');
+    });
+
+    it('asserts top alignment', () => {
+      cy.get('section[aria-labelledby="Top-stories"] li:nth-child(1) > div > div > div > div > img')
+        .should('be.topAligned', 'section[aria-labelledby="Top-stories"] li:nth-child(1) > div > div > h3')
+        .should('not.be.topAligned', '#root > header > nav > div > ul > li:nth-child(9)');
+    });
+
+    it('asserts bottom alignment', () => {
+      cy.get('section[aria-labelledby="Top-stories"] li:nth-child(1) > div > div > div > div > img')
+        .should('be.bottomAligned', 'section[aria-labelledby="Top-stories"] li:nth-child(1) > div > div')
+        .should('not.be.bottomAligned', '#root > header > nav > div > ul > li:nth-child(9)');
+    });
+  });
+
+  context('Positioning', () => {
+    it('asserts left of', () => {
+      cy.get('section[aria-labelledby="Top-stories"] li:nth-child(1) > div > div > div > div > img')
+        .should('be.leftOf', 'section[aria-labelledby="Top-stories"] li:nth-child(1) h3', 16)
+        .should('not.be.leftOf', '#root > header > nav > div > ul > li:nth-child(9)', 16);
+    });
+
+    it('asserts right of', () => {
+      cy.get('section:nth-child(1) > div > ul > li:nth-child(1) > div > div > h3')
+        .should('be.rightOf', 'section[aria-labelledby="Top-stories"] li:nth-child(1) > div > div', 16)
+        .should('not.be.rightOf', '#root > header > nav > div > ul > li:nth-child(9)', 16);
+    });
+
+    it('asserts above', () => {
+      cy.get('section:nth-child(1) > div > ul > li:nth-child(1)')
+        .should('be.above', 'section:nth-child(1) > div > ul > li:nth-child(2)', 32)
+        .should('not.be.above', '#root > header > nav > div > ul > li:nth-child(9)', 32);
+    });
+  });
 });
--- index.js
+++ index.js
@@ -1,17 +1,55 @@
 /* eslint-disable no-underscore-dangle */
 require('./lib');
 
+// the rect returned by el.getBoundingClientRect() isn't writable, which is a
+// pain. This produces a facsimile of the rect that is.
+function getBoundingClientRect(element) {
+  const rect = element.getBoundingClientRect();
+  return {
+    top: rect.top,
+    right: rect.right,
+    bottom: rect.bottom,
+    left: rect.left,
+    width: rect.width,
+    height: rect.height,
+    x: rect.x,
+    y: rect.y,
+  };
+}
+
+// getBoundingClientRect returns a rect that incorporates padding, we won't
+// necessarily want this, if config.removePadding = true we remove it.
+function removePadding(object, rect) {
+  const dimensions = Object.assign({}, rect);
+  const cs = getComputedStyle(object);
+  dimensions.left += parseFloat(cs.paddingLeft);
+  dimensions.right -= parseFloat(cs.paddingRight);
+  dimensions.top -= parseFloat(cs.paddingTop);
+  dimensions.bottom += parseFloat(cs.paddingBottom);
+  dimensions.width -= parseFloat(cs.paddingLeft) + parseFloat(cs.paddingRight);
+  dimensions.height -= parseFloat(cs.paddingTop) + parseFloat(cs.paddingBottom);
+  dimensions.x += parseFloat(cs.paddingLeft);
+  dimensions.y += parseFloat(cs.paddingTop);
+  return dimensions;
+}
+
 function getRect(subject) {
+  let element;
+
   if (subject === '&document') {
-    return Cypress.$(document).context.documentElement.getBoundingClientRect();
+    element = Cypress.$(document).context.documentElement;
+  } else if (typeof subject === 'string') { // the selector passed in to assertion
+    [element] = Cypress.$(subject);
+  } else if (typeof subject === 'object') { // the element from cy.get() i.e this._obj
+    [element] = subject;
+  } else {
+    element = null; // something unknown
   }
-  if (typeof subject === 'string') { // the selector passed in to assertion
-    return Cypress.$(subject)[0].getBoundingClientRect();
-  }
-  if (typeof subject === 'object') { // the element from cy.get() i.e this._obj
-    return subject[0].getBoundingClientRect();
-  }
-  return null; // something unknown
+  const rect = getBoundingClientRect(element);
+
+  if (Cypress.config('removePadding')) {
+    return removePadding(element, rect);
+  } return element.getBoundingClientRect();
 }
 
 function getRects(first, second) {
@@ -20,7 +58,56 @@ function getRects(first, second) {
   return [actual, expected];
 }
 
-const aligned = (_chai, utils) => {
+const positioned = (_chai) => {
+  function leftOf(element, measure) {
+    const rects = getRects(this._obj, element);
+    const calc = () => -(rects[0].right - rects[1].left);
+    this.assert(
+      calc() === measure,
+      `expected ${this._obj.selector} to be left of ${element} by #{exp} but got #{act}`,
+      `expected ${this._obj.selector} to not be left of ${element} by #{exp} but got #{act}`,
+      measure,
+      calc(),
+    );
+  }
+
+  function rightOf(element, measure) {
+    const rects = getRects(this._obj, element);
+    const calc = () => rects[0].left - rects[1].right;
+    this.assert(
+      calc() === measure,
+      `expected ${this._obj.selector} to be right of ${element} by #{exp} but got #{act}`,
+      `expected ${this._obj.selector} to not be right of ${element} by #{exp} but got #{act}`,
+      measure,
+      calc(),
+    );
+  }
+
+  const { Assertion } = chai;
+  Assertion.overwriteMethod('above', _super => function assertAbove(element, measure) {
+    const obj = this._obj;
+    if (typeof obj === 'object') {
+      const rects = getRects(obj, element);
+      const calc = () => rects[0].bottom - rects[1].top;
+      this.assert(
+        calc() === measure,
+        `expected ${this._obj.selector} to be above ${element} by #{exp} but got #{act}`,
+        `expected ${this._obj.selector} to not be above ${element} by #{exp} but got #{act}`,
+        measure,
+        calc(),
+      );
+    } else {
+      _super.apply(this, arguments);
+    }
+  });
+
+  _chai.Assertion.addMethod('leftOf', leftOf);
+  _chai.Assertion.addMethod('rightOf', rightOf);
+  // _chai.Assertion.addMethod('above', above);
+  // _chai.Assertion.addMethod('below', below);
+};
+
+const aligned = (_chai) => {
   function leftAligned(element) {
     const rects = getRects(element, this._obj);
     this.assert(
@@ -68,3 +155,4 @@ const aligned = (_chai, utils) => {
 };
 
 chai.use(aligned);
+chai.use(positioned);
